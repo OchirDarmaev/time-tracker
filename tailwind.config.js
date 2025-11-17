106177d3c11b538baabcf434fa925be157529532
Ochir Darmaev <ochirdarmaev@gmail.com>
2025-11-17 18:47:24 +0300

    Enhance CSS build process and integrate Tailwind CSS
    - Updated package.json scripts to include CSS build commands using PostCSS and Tailwind CSS.
    - Added postcss.config.js and tailwind.config.js for Tailwind CSS configuration.
    - Introduced new CSS files for different components and served them statically in the Express server.
    - Removed direct Tailwind CDN usage in HTML templates, replacing it with local CSS file links for better performance and customization.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{ts,html,js}",
+    "./dist/**/*.{js,html}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
